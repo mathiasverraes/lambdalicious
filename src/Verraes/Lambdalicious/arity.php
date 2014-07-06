669cc52c16d864b779fd769b70748a161e692ba8
Mathias Verraes <mathias@verraes.net>
2014-07-06 21:23:35 +0200

    credit where credit is due

--- src/Verraes/Lambdalicious/arity.php
+++ src/Verraes/Lambdalicious/arity.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * From http://jasonframe.co.uk/logfile/2009/05/finding-the-arity-of-a-closure-in-php-53/
+ * @param $lambda
+ * @return int
+ */
 function arity($lambda)
 {
     $m = (new ReflectionObject($lambda))->getMethod('__invoke');
