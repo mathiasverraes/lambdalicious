6fdcdb659307082fadd02a7b4bd36c881eeac895
Mathias Verraes <mathias@verraes.net>
2014-09-04 09:50:21 +0200

    dogfood the new and improved partial

--- examples/04-fibonacci.php
+++ examples/04-fibonacci.php
@@ -25,8 +25,8 @@ assert(
 
 // If we want to use cond, we need to wrap add(fib(n-1), fib(n-2)) in a closure. That makes the whole thing rather ugly.
 // I'm using $decrement here for fun
-$decrement = partial(reverseargs(subtract), 1); // smarter partial would be better
-$fib2 = function($n) use (&$fib2, $decrement) {
+$decrement = partial(subtract, __, 1);
+$fib2 = memoize(function($n) use (&$fib2, $decrement) {
     return cond(
         pair(lt($n, 2), $n),
         pair(
@@ -36,7 +36,7 @@ $fib2 = function($n) use (&$fib2, $decrement) {
             }
         )
     );
-};
+});
 assert(
     $fib2(12) == 144
 );
