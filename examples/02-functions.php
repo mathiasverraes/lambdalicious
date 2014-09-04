4d5e72a6dceb487b4d7d03961d7591733e88f2f3
Mathias Verraes <mathias@verraes.net>
2014-09-04 10:17:08 +0200

    How fast can you fib?

--- examples/02-functions.php
+++ examples/02-functions.php
@@ -8,18 +8,19 @@ assert(
     $second([a, b, c])
 );
 
+// Filter a list using a predicate (a function that returns a boolean)
+$listOfLists = [[a, b], [], [c, d], []];
+assert(
+    filter(not(isempty), $listOfLists) === [[a, b], [c, d]]
+);
+
 // partial fixes a number of arguments of a function. eg add(x, y) takes two arguments, partial(add, 1) creates a function add1(y)
 $add1 = partial(add, 1);
 assert(
     $add1(2) === 3
 );
 
-$listOfLists = [[a, b], [], [c, d]];
-assert(
-    filter(not(isempty), $listOfLists) === [[a, b], [c, d]]
-);
-
-// Another example of partial function application, both expressions evaluate the same
+// Another example of partial function application
 $removeEmptyLists = partial(filter, not(isempty));
 assert(
     $removeEmptyLists($listOfLists)  === [[a, b], [c, d]]
--- examples/04-fibonacci.php
+++ examples/04-fibonacci.php
@@ -3,27 +3,30 @@
 require_once __DIR__ . '/../src/Verraes/Lambdalicious/preload.php';
 
 
+// It's is strictly forbidden to do functional programming without calculating Fibonacci numbers
 $fib = function($n) use(&$fib) {
     return lt($n, 2) ? $n : add($fib($n-1), $fib($n-2));
 };
-
-// It's is strictly forbidden to do functional programming without calculating Fibonacci numbers
 assert(
     $fib(12) == 144
 );
 
+// Memoization can help with expensive recursive operations
 $fastfib = memoize(function($n) use(&$fastfib) {
     return lt($n, 2) ? $n : add($fastfib($n-1), $fastfib($n-2));
 });
 assert(
     $fastfib(12) == 144
 );
-// Memoization can help with expensive recursive operations
-// echo profile($fastfib, 100) . PHP_EOL;
-// echo profile($fib, 100) . PHP_EOL;
+// You may need to increase "xdebug.max_nesting_level" in the php.ini for these:
+// $n = 28;
+// echo "fastfib($n) duration: " . profile($fastfib, $n) . PHP_EOL; // 0.0004s on my machine
+// echo "fib($n) duration: " . profile($fib, $n) . PHP_EOL; // 3.4487s
 
 
-// If we want to use cond, we need to wrap add(fib(n-1), fib(n-2)) in a closure. That makes the whole thing rather ugly.
+// If we want to use cond, we need to wrap add(fib(n-1), fib(n-2)) in a closure. That makes the whole thing rather ugly,
+// so in practice, ifs or ternaries are still recommend. If the second expression in a pair is a nullary function, it
+// will be evaluated lazily by cond.
 // I'm using $decrement here for fun
 $decrement = partial(subtract, __, 1);
 $fib2 = memoize(function($n) use (&$fib2, $decrement) {
