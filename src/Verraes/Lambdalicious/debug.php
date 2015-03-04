e680d372b686a9c186d207709bca2ebf1d0db8c7
turanct <spinnewebber_toon@hotmail.com>
2015-03-04 11:06:48 +0100

    Remove duplication within dump/pretty

--- src/Verraes/Lambdalicious/debug.php
+++ src/Verraes/Lambdalicious/debug.php
@@ -11,7 +11,20 @@ atom(@pretty);
  */
 function dump(...$args)
 {
-    print(implode("\n", la(map(pretty, al($args)))));
+    print(_prettyList(al($args), "\n"));
+}
+
+/**
+ * Return a string representation of a list
+ *
+ * @param list   $list
+ * @param string $delimiter
+ *
+ * @return string
+ */
+function _prettyList($list, $delimiter = ', ')
+{
+    return implode($delimiter, la(map(pretty, $list)));
 }
 
 /**
@@ -24,7 +37,7 @@ function dump(...$args)
 function pretty($data)
 {
     return
-        islist($data) ? '(' . implode(', ', la(map(pretty, $data))) . ')' :
+        islist($data) ? '(' . _prettyList($data) . ')' :
         (ispair($data) ? '(' . pretty(head($data)) . ' . ' . pretty(tail($data)) . ')':
         print_r($data, true))
     ;
